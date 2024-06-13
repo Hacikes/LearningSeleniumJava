5218deed34ce8d97dbfff3570a837302329a407e
hacikes <volna475@mail.ru>
2024-06-13 14:08:36 +0300

    Добавил файл с тестовым классом и добавил вебдрайвер хрома

---
+++ src/test/java/com/tmd/tests/LoginPageTests.java
@@ -0,0 +1,11 @@
+package com.tmd.tests;
+
+import org.testng.annotations.Test;
+
+@Test
+public class LoginPageTests {
+
+    public void test1() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+    }
+}
